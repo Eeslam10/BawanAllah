39ac1affedeed165c88be9930c1c648f840531e3
Islam <islammuhd8@gmail.com>
2024-06-29 18:33:41 -0700

    footer completed

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,39 +1,35 @@
 import React from 'react';
-import './Footer.css'
 import { FaFacebook, FaTwitter, FaInstagram } from 'react-icons/fa';
-import FB from '../../assets/fb.svg';
-import IG from '../../assets/ig.svg';
-import twitter from '../../assets/twitter.svg';
 import FooterLogo from '../../assets/footer_logo.svg';
 
 
 const Footer = () => {
   return (
     <div className='footer'>
-        <div className="box">
+        <div className="box flex justify-between m-6 pb-4">
             <div>
                 <img src={FooterLogo} alt="wareview" />
             </div>
-            <div>         
-                <a href="">Dashboard</a>
-                <a href="">Sales</a>
-                <a href="">Banking</a>
-                <a href="">Records</a>
-                <a href="">Contact Us</a>
+            <div className='flex gap-12 font-semibold '>         
+                <a href="" className='hover:underline'>Dashboard</a>
+                <a href="" className='hover:underline'>Sales</a>
+                <a href="" className='hover:underline'>Banking</a>
+                <a href="" className='hover:underline'>Records</a>
+                <a href="" className='hover:underline'>Contact Us</a>
             </div>
         </div>
-        <div className="box">
+        <div className="box flex justify-between m-6">
             <div>
                 <p>Cultivating Efficency, Tracking Excellence - <br /> Your Trusted Inventory Management Partner</p>
             </div>      
-            <div className='social_links space-x-6 '>
-                <img src={FB} alt="facebook" />
-                <img src={IG} alt="instagram" />
-                <img src={twitter} alt="twitter" />
+            <div className='social_links space-x-6 flex'>
+                <FaTwitter size={30} color='#33A6FF'className='cursor-pointer'/>
+                <FaFacebook size={30} color='#33A6FF'className='cursor-pointer'/>
+                <FaInstagram size={30} color='#33A6FF'className='cursor-pointer'/>
             </div>
-            {/* <hr /> */}
         </div>
-        <div className="box">
+            <hr className='border-2 mx-4'/>
+        <div className="box flex justify-between m-6" >
             <p>Privacy Policy</p>
             <p>&copy; 2023 Mugna Technologies, inc</p>
             <p>Terms & Conditions</p>
